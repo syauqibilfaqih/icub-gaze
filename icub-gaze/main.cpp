8b6d9443d0d42dbaaf4e8eebc6fe92b4ef85493a
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2024-12-20 11:53:21 +0100

    Put sphere to the world

--- icub-gaze/main.cpp
+++ icub-gaze/main.cpp
@@ -8,28 +8,54 @@ using namespace yarp::sig;
 
 int main(int argc, char *argv[]) {
     Network yarp; // initialize the network
-    RpcClient port; // initialize Rpc port as client
+    RpcClient portRpc; // initialize Rpc port as client
 
-    port.open("/icub-sender"); // open a new port
-    yarp.connect("/icub-sender","/icubSim/head/rpc:i"); // connect to the head's port
+    portRpc.open("/sphere"); // open a new port for sphere
+    yarp.connect("/sphere","/icubSim/world"); // connect to the head's port
 
+    // create a new sphere on the window
+    
+    // create a new bottle for producing sphere and change its position
+    Bottle sphereReq;
+
+    // clear every entities in case it's exist in the previous run
+    sphereReq.addString("world");
+    sphereReq.addString("del");
+    sphereReq.addString("all");
+    portRpc.write(sphereReq);
+
+    sphereReq.clear();
+
+    // send the command  `world mk ssph (radius) (three params for pos) (three params for colour)` through bottle
+    // with values world mk ssph 0.04 0.0 0.9 0.8 1 0 0 
+    sphereReq.addString("world");
+    sphereReq.addString("mk");
+    sphereReq.addString("ssph");
+    sphereReq.addFloat64(0.04);
+    sphereReq.addFloat64(0.0);
+    sphereReq.addFloat64(0.9);
+    sphereReq.addFloat64(0.8);
+    sphereReq.addFloat64(1.0);
+    sphereReq.addFloat64(0);
+    sphereReq.addFloat64(0);
+    portRpc.write(sphereReq);
 
     // Sending command to move the robot's head in a loop
-    for(int i=-30; i<31; i++){
-        for(int j=0; j<6; j++){
-            Bottle req; // using Bottle as the message's data type
-            req.addString("set");
-            req.addString("pos");
-            req.addInt8(j);
-            req.addInt8(i);
-            port.write(req);
-        }
-        Time::delay(0.1);
-    }
+    // for(int i=-30; i<31; i++){
+    //     for(int j=0; j<6; j++){
+    //         Bottle req; // using Bottle as the message's data type
+    //         req.addString("set");
+    //         req.addString("pos");
+    //         req.addInt8(j);
+    //         req.addInt8(i);
+    //         port.write(req);
+    //     }
+    //     Time::delay(0.1);
+    // }
 
     
 
-    port.close(); // closing the port after its finished
+    portRpc.close(); // closing the port after its finished
 
     return 0;
 }
